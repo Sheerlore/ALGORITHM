f3e8b536c00ad8c6ea67623618758de50fd3d617
uai <noema01@outlook.jp>
2020-07-14 23:18:14 +0900

    add modify fileread.c

--- SMALL/fileread.c
+++ SMALL/fileread.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 10001
 
 int main() {
     FILE *fp;
-    if((fp = fopen("./input.txt", "r")) != NULL){
-        puts("fp != NULL");
-    }else {
-        puts("fp == NULL");
-    }
+    char data[N][80];
+    unsigned int i, j;
 
+    if((fp = fopen("./input.txt", "r")) == NULL) exit(1);
+
+    for(i = 0; i < 10000 && fgets(data[i], sizeof(data[i]), fp) != NULL;i++){
+        printf("data[%d]=%s", i, data[i]);
+        for(j = 0;fscanf(fp, "%c", &data[i][j]) != EOF;j++){
+            printf("%c\n", data[i][j]);
+        }
+    }
+    
     fclose(fp);
 
     return 0;
